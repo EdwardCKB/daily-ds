1958488f13542b1902e27cac9df7a964cc0b87cf
Edward <edwardchew696@gmail.com>
2026-08-03 22:59:38 +0800

    add hyperparameter tuning with GridSearchCV for random forest

--- clean-code/titanic_random_forest.py
+++ clean-code/titanic_random_forest.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 
 def build_pipeline():
@@ -14,19 +15,32 @@ def build_pipeline():
     ])
     pipeline = Pipeline(steps=[
         ('preprocess', preprocessor),
-        ('model', RandomForestClassifier())
+        ('model', RandomForestClassifier(random_state=42))
     ])
     return pipeline
+
+def hyperparameter_tunning(pipeline, X_train, y_train):
+    """Construcing hyperparameter tunning to get model's best hyperparamters"""
+    param_grid = {
+        'model__n_estimators':[50, 100, 150, 200],
+        'model__max_depth':[4, 6, 8],
+        'model__min_samples_split':[2, 5, 10]
+    }
+    grid_sesarch = GridSearchCV(pipeline, param_grid, cv=5, scoring='f1')
+    grid_sesarch.fit(X_train, y_train)
+    return grid_sesarch.best_estimator_
+
 
 def main():
     df = load_data("data/train.csv")
     df = clean_data(df)
     X_train, X_test, y_train, y_test = train_test_split_data(df, 0.2)
     pipeline = build_pipeline()
-    pipeline.fit(X_train, y_train)
-    pred = pipeline.predict(X_test)
+    best_model = hyperparameter_tunning(pipeline, X_train, y_train)
+    pred = best_model.predict(X_test)
     accuracy, precision, recall, f1, matrix = matrix_calculation(y_test, pred)
     print(f"Accuracy: {accuracy}\nPrecision: {precision}\nRecall: {recall}\nF1: {f1}\nmatrix: {matrix}")
+    print(best_model.named_steps['model'].get_params())
 
 if __name__ == "__main__":
     main()
